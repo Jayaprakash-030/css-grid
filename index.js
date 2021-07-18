0215b5182b0f35c57081001200bc4c36fd686db4
Jayaprakash-030 <63773533+Jayaprakash-030@users.noreply.github.com>
2021-07-19 00:56:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("button");
+const list = document.querySelector(".Academics");
+
+button.addEventListener("click",() => {
+    if(list.style.display === "none")
+        list.style.display = "grid";
+    else
+        list.style.display = "none";
+});
+
+    // i i i i j j j j k k k k
+    // l l l l m m m m n n n n
+    // o o o o o o p p p p p p
